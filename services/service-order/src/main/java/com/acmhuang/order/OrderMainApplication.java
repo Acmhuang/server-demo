675bf0efa029fa732f284a3d322677cb04f83f1c
chengming <118154550+Acmhuang@users.noreply.github.com>
2025-09-17 19:49:26 +0800

    使用NacosConfigManager监听配置变化
    Introduced a bean that registers a listener for changes to the 'service-order.yml' configuration in Nacos. The listener uses a fixed thread pool executor and logs configuration changes, simulating an email notification.

--- services/service-order/src/main/java/com/acmhuang/order/OrderMainApplication.java
+++ services/service-order/src/main/java/com/acmhuang/order/OrderMainApplication.java
@@ -1,7 +1,15 @@
 package com.acmhuang.order;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * @author Acmhuang
@@ -11,5 +19,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderMainApplication.class, args);
+    }
+
+
+    /**
+     * 监听nacos配置文件变化
+     * @param manager
+     * @return
+     */
+    @Bean
+    ApplicationRunner applicationRunner(NacosConfigManager manager) {
+        return args -> {
+            ConfigService configService = manager.getConfigService();
+            configService.addListener(
+                    "service-order.yml",
+                    "DEFAULT_GROUP",
+                    new Listener() {
+                        @Override
+                        public Executor getExecutor() {
+                            return Executors.newFixedThreadPool(7);
+                        }
+
+                        @Override
+                        public void receiveConfigInfo(String configInfo) {
+                            System.out.println("变化的配置信息" + configInfo);
+                            System.out.println("邮件通知...");
+                        }
+                    }
+            );
+            System.out.println("==================================");
+        };
     }
 }
