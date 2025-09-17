5e6d9023ef2d69b96603daf63ccb611d05956235
chengming <118154550+Acmhuang@users.noreply.github.com>
2025-09-17 18:53:10 +0800

    配置中心基本用法
    Added spring-cloud-starter-alibaba-nacos-config dependency and enabled Nacos config import in application.yml. Updated OrderController with @RefreshScope and new /config endpoint to expose orderTimeout and orderAutoConfirm properties.

--- services/service-order/src/main/java/com/acmhuang/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/acmhuang/order/controller/OrderController.java
@@ -3,18 +3,34 @@ package com.acmhuang.order.controller;
 import com.acmhuang.order.bean.Order;
 import com.acmhuang.order.service.OrderService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Acmhuang
  * @date 2025/09/17 14:13
  **/
+//激活配置文件刷新
+@RefreshScope
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
     @Resource
     private OrderService orderService;
+
+    @Value("${order:timeout}")
+    private String orderTimeout;
+
+    @Value("${order:auto-confirm}")
+    private String orderAutoConfirm;
+
+    @GetMapping("/config")
+    public String config(){
+        return "orderTimeout:"+orderTimeout+"; orderAutoConfirm:"+orderAutoConfirm;
+    }
+
 
     @GetMapping("/create")
     public Order getOrder(@RequestParam("productId") Long productId,@RequestParam("userId") Long userId) {
