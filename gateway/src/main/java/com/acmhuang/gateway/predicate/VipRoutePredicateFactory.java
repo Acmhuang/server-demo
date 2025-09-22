8e415a222b1622d16f735dd73e8480eb9ee7f959
chengming <118154550+Acmhuang@users.noreply.github.com>
2025-09-22 20:01:30 +0800

    Gateway断言-自定义断言工厂与断言写法
    Introduces a custom VipRoutePredicateFactory for Spring Cloud Gateway to enable route matching based on specific query parameters. Updates application-route.yml to use the new predicate and refactors predicate definitions to use the expanded YAML format.

---
+++ gateway/src/main/java/com/acmhuang/gateway/predicate/VipRoutePredicateFactory.java
@@ -0,0 +1,75 @@
+package com.acmhuang.gateway.predicate;
+
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.cloud.gateway.handler.predicate.AbstractRoutePredicateFactory;
+import org.springframework.cloud.gateway.handler.predicate.GatewayPredicate;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * @author Acmhuang
+ * @date 2025/09/22 19:43
+ **/
+@Component
+public class VipRoutePredicateFactory extends AbstractRoutePredicateFactory<VipRoutePredicateFactory.Config> {
+
+    public VipRoutePredicateFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public Predicate<ServerWebExchange> apply(Config config) {
+        return new GatewayPredicate() {
+            @Override
+            public boolean test(ServerWebExchange serverWebExchange) {
+                // localhost/search?q=haha&user=admin
+                ServerHttpRequest request = serverWebExchange.getRequest();
+                String first = request.getQueryParams().getFirst(config.param);
+                return StringUtils.hasText(first) && first.equals(config.value);
+            }
+        };
+    }
+
+    // 为短写法准备的指定参数顺序
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList("param", "value");
+    }
+
+    /**
+     * 配置参数
+     */
+    @Validated
+    public static class Config {
+
+        @NotEmpty
+        private String param;
+
+        @NotEmpty
+        private String value;
+
+        public @NotEmpty String getParam() {
+            return param;
+        }
+
+        public void setParam(@NotEmpty String param) {
+            this.param = param;
+        }
+
+        public @NotEmpty String getValue() {
+            return value;
+        }
+
+        public void setValue(@NotEmpty String value) {
+            this.value = value;
+        }
+    }
+}
